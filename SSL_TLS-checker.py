331d6f5d5c060b4c8ac3f0cf4a1feb48328a232f
Ayushi Chaudhuri <61285140+IUC4801@users.noreply.github.com>
2023-01-08 20:22:26 +0530

    added new file

---
+++ SSL_TLS-checker.py
@@ -0,0 +1,67 @@
+import ssl
+import socket
+import argparse
+
+
+def check_vulnerabilities(hostname, port):
+    # Create an SSL context
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    context.set_ciphers('HIGH')
+    context.options |= ssl.OP_NO_SSLv2
+    context.options |= ssl.OP_NO_SSLv3
+
+    # Create a socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Wrap the socket in an SSL/TLS context
+    ssl_sock = context.wrap_socket(sock, server_hostname=hostname)
+
+    # Connect to the server
+    ssl_sock.connect((hostname, port))
+
+    # Check for vulnerabilities
+    vulnerabilities = []
+    if context.check_hostname:
+        try:
+            ssl.match_hostname(ssl_sock.getpeercert(), hostname)
+        except Exception as e:
+            vulnerabilities.append(str(e))
+    if context.verify_mode == ssl.CERT_NONE:
+        vulnerabilities.append("certificate verification is disabled")
+    if ssl_sock.cipher()[1] < 128:
+        vulnerabilities.append("weak cipher suite is being used")
+
+    # Check for supported protocols
+    protocols = ssl_sock.supported_protocols()
+    if "TLSv1.2" not in protocols:
+        vulnerabilities.append("TLS 1.2 is not supported")
+    if "TLSv1.3" not in protocols:
+        vulnerabilities.append("TLS 1.3 is not supported")
+
+    # Close the socket
+    ssl_sock.close()
+
+    return vulnerabilities
+
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hostname", help="the hostname of the website")
+    parser.add_argument("-p", "--port", default=443, type=int,
+                        help="the port number (default: 443)")
+    args = parser.parse_args()
+
+    # Check for vulnerabilities
+    vulnerabilities = check_vulnerabilities(args.hostname, args.port)
+
+    if vulnerabilities:
+        print("Vulnerabilities found:")
+        for vulnerability in vulnerabilities:
+            print(" - " + vulnerability)
+    else:
+        print("No vulnerabilities found")
+
+
+if __name__ == "__main__":
+    main()
